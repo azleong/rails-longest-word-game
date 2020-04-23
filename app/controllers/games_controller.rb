88c20451d70d4972d004acb2b2410ea4edc1f0c5
Andy Leong <mindreko@gmail.com>
2020-04-23 20:33:12 +1000

    more words

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,21 @@
 class GamesController < ApplicationController
 
 def new
+   @letters = Array.new(9) { ('A'..'Z').to_a.sample }
 end
   
 def score
+    @word = params[:word]
+    @letters = params[:letters]
+    if included?(@word, @letters)
+      [score, "well done"]
+  else
+    	[0, "not in the grid"]
+  end
 end
 
+def included?(guess, grid)
+    guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) } 
+end
+	
 end
